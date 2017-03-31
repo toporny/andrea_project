751d41310233e4d0bcdf916285fa26568a659db6
toporny <toporny@gmail.com>
2017-04-01 00:58:15 +0100

    gulp, gulp-sass, gulp-connect, gulp-template-html implemented

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var template = require('gulp-template-html');
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
+});
+
+// keeps gulp from crashing for scss errors
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+      .pipe(sass({ errLogToConsole: true }))
+      .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('livereload', function (){
+  gulp.src('./public/**/*')
+  .pipe(connect.reload());
+});
+
+gulp.task('template', function (){
+ gulp.src('html_content/*.html')
+      .pipe(template('html_templates/template.html'))
+      .pipe(gulp.dest('public'));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch(['./html_content/**/*.html','./html_templates/**/*.html'], ['template']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./public/*', ['livereload']);
+});
+
+gulp.task('default', ['connect', 'watch', 'sass', 'template']);
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://gist.github.com/kellishouts/2c7cde625491ad53fb3c
